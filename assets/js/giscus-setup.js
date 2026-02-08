847ef298a74130ba4f7f4f801c8bdef4f8b1fd12
tejasstanley <140648660+tejasstanley@users.noreply.github.com>
2026-02-08 03:50:31 +0000

    Deploying to gh-pages from @ tejasstanley/tejasstanley.github.io@e90224dd9cc4fbb5fcfb0e3067bcd68cc81ebe00 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "tejasstanley/tejasstanley",
+    "data-repo": "tejasstanley/tejasstanley.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
